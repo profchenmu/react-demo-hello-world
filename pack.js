8717c64ac7ad012ead7ea4184f5885a4ac778bcf
linyuhong <linyuhong@douyole.com>
2016-08-16 18:16:29 +0800

    small fix

--- pack.js
+++ pack.js
@@ -3,10 +3,10 @@ module.exports = {
     enterPath: './app/index.js',
     tplPath: './app/index.tpl.html',
     plugins: [
-    	new webpack.ProvidePlugin({
+    	{
     	    $: "jquery",
     	    jQuery: "jquery",
     	    "window.jQuery": "jquery"
-    	})
+    	}
     ]
 };
